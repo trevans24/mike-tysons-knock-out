f7487c501aa4fcf88d855680e7430696d5639fa8
trevans24 <trevans24@gmail.com>
2017-03-27 17:49:21 -0600

    drawing gloves on canvas, changed buttons to match 8bit styling

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 var canvas = document.getElementById('game-board'),
 	ctx = canvas.getContext('2d'),
-	background = new Image();
+	background = new Image(),
+	leftHand = new Image(),
+	rightHand = new Image();
 
 canvas.width = 1024;
 canvas.height = 600;
 
 background.src = "images/boxing-ring.png";
+leftHand.src = "images/boxing-glove-left.png";
+rightHand.src = "images/boxing-glove-right.png";
 
 background.onload = function(){
 	ctx.drawImage(background,0,0);
+	ctx.drawImage(leftHand, 400, 500, 100, 100);
+	ctx.drawImage(rightHand, 520, 500, 100, 100);
 };
